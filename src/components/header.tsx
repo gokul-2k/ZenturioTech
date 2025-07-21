73fad1649a94502e895e08574db192dbc25797d5
gokul-2k <119345021+gokul-2k@users.noreply.github.com>
2025-07-21 12:14:44 +0530

    web/app

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -394,7 +394,7 @@ export default function Header() {
                   closeMobileDropdown={() => setMobileServicesDropdownOpen(false)}
                 />
                 <DropdownItem 
-                  label="Web & App" 
+                  label="Web/Mobile" 
                   path="/services/web" 
                   closeDesktopDropdown={() => setServicesDropdownOpen(false)}
                   closeMobileDropdown={() => setMobileServicesDropdownOpen(false)}
